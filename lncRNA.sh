74d8b1461bf408f5f07ea03045dc62862eef3b7f
Milos Pjanic <pjanic.milos@gmail.com>
2019-01-03 14:53:10 -0800

    Update lncRNA.sh

--- lncRNA.sh
+++ lncRNA.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-number_rep = $1
-min_tr_lenght = $2
-min_FPKM = $3
-min_isoform_fract = $4
-min_gap = $5
+number_rep=$1
+min_tr_lenght=$2
+min_FPKM=$3
+min_isoform_fract=$4
+locus_gap=$5
+transcript_merge_gap=$6
+
+BAM_DIR=$7
 
 SECONDS=0
-$BAM_DIR=
+
 ###fastqc quality control - requires fastqc installed and placed in PATH
 
 mkdir GTFs
@@ -34,7 +37,7 @@ samtools index merged.bam
 ls -l merged.bam merged.bam.bai
 
 echo "Stringtie analysis of combined conditions.."
-stringtie -l MergedBAM -p 64 -o GTFs/mergedBam.gtf mergedBam_sort.bam
+stringtie -l MergedBAM -p 64 -m $min_tr_lenght -f $min_isoform_fract -g $locus_gap -o GTFs/mergedBam.gtf mergedBam_sort.bam
 
 duration=$SECONDS
 echo "Stringtie analysis of combined conditions: $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." > mergedBam.tm
@@ -69,7 +72,7 @@ sed 's/samtools sort -o /samtools index /g' merged.sort.name >> per.condition.sh
 sed -i 's/samtools sort -o //g' merged.sort.name
 
 sed 's/$/.merged.perCond.gtf/g' merged.name > merged.name.gtf
-sed -i 's/^/-p 64 -o GTFs\//g' merged.name.gtf 
+sed -i 's/^/-p 64 -m $min_tr_lenght -f $min_isoform_fract -g $locus_gap -o GTFs\//g' merged.name.gtf 
 sed 's/^/stringtie -l /g' merged.name > begin.tmp
 
 paste -d " " begin.tmp merged.name.gtf merged.sort.name >> per.condition.sh
@@ -91,13 +94,13 @@ SECONDS=0
 
 printf "Downloading reference annotation..\n\n"
 wget ftp://ftp.ebi.ac.uk/pub/databases/RNAcentral/releases/10.0/genome_coordinates/gff3/homo_sapiens.GRCh38.gff3.gz
-gunzip genome_coordinates/gff3/homo_sapiens.GRCh38.gff3.gz
+gunzip homo_sapiens.GRCh38.gff3.gz
 
 printf "Merging GTFs: combined condition and condition specific..\n\n"
 cd GTFs
 
-stringtie --merge -l CombGTF -p 64 -G genome_coordinates/gff3/homo_sapiens.GRCh38.gff3 -m $min_tr_lenght -F $min_FPKM -f $min_isoform_fract -g $min_gap -i -o ./mergedGtf.gtf *merged.perCond.gtf
-stringtie --merge -l CombGTF -p 64 -G genome_coordinates/gff3/homo_sapiens.GRCh38.gff3 -m $min_tr_lenght -F $min_FPKM -f $min_isoform_fract -g $min_gap -i -o ./CombGTF.gtf mergedBam.gtf mergedGtf.gtf 
+stringtie --merge -l CombGTF -p 64 -G homo_sapiens.GRCh38.gff3 -m $min_tr_lenght -F $min_FPKM -f $min_isoform_fract -g $transcript_merge_gap -i -o ./mergedGtf.gtf *merged.perCond.gtf
+stringtie --merge -l CombGTF -p 64 -G homo_sapiens.GRCh38.gff3 -m $min_tr_lenght -F $min_FPKM -f $min_isoform_fract -g $transcript_merge_gap -i -o ./CombGTF.gtf mergedBam.gtf mergedGtf.gtf 
 
 printf "Number of discovered transcripts in combined conditions transcriptome:\n\n"
 grep "	transcript	"  mergedBam.gtf | wc -l
